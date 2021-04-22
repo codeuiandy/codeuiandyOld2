dbac4853cd69df4967a9759fc2605718296f63e3
Abydin <Abideenayinde7@gmail.com>
2021-04-22 15:48:25 +0100

    added the homepage components

--- src/layout/navbar.jsx
+++ src/layout/navbar.jsx
@@ -9,9 +9,30 @@ export default function Navbar() {
         </div>
 
         <div className="navbar-links">
-          <span>link 1</span>
-          <span>link 1</span> <span>link 1</span> <span>link 1</span>{" "}
-          <span>link 1</span>
+          <ul className="codeuiandy-nav">
+            <li>
+              <a href="">
+                <span className="codeuiandy-nav-no">01.</span> About
+              </a>
+            </li>
+            <li>
+              <a href="">
+                <span className="codeuiandy-nav-no">02.</span> Experience
+              </a>
+            </li>
+            <li>
+              <a href="">
+                <span className="codeuiandy-nav-no">03.</span> Work
+              </a>
+            </li>
+            <li>
+              <a href="">
+                <span className="codeuiandy-nav-no">04.</span> Contact
+              </a>
+            </li>
+          </ul>
+
+          <span className="resume-codeuiandy">Resume</span>
         </div>
       </div>
     </div>
