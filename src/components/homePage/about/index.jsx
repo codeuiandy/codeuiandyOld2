fb9112236adecb19483dfbf5d612ad5e90ff14de
Abydin <Abideenayinde7@gmail.com>
2021-04-23 12:20:04 +0100

    started youtube section

--- src/components/homePage/about/index.jsx
+++ src/components/homePage/about/index.jsx
@@ -1,6 +1,15 @@
-import React from "react";
+import React, { useRef, ref } from "react";
+import codeUiAndyImg from "../../../assets/images/image-of-code-ui-andy.jpg";
 
 const Index = () => {
+  const targetImageBorder = useRef();
+  const moveImageBorder = () => {
+    targetImageBorder.current.className = "animatate-codeuiandy-img-border";
+  };
+
+  const RevertmoveImageBorder = () => {
+    targetImageBorder.current.className = "codeuiandy-image-border ";
+  };
   return (
     <div>
       <div className="aboutmeSectionDiv">
@@ -45,7 +54,19 @@ const Index = () => {
             </div>
           </div>
         </div>
-        <div className="a-photo-of-codeuiandy"></div>
+        <div className="a-photo-of-codeuiandy">
+          <img
+            onMouseEnter={moveImageBorder}
+            onMouseOut={RevertmoveImageBorder}
+            src={codeUiAndyImg}
+            alt="Image of codeuiandy"
+            className="codeuiandy-image"
+          />
+          <div
+            ref={targetImageBorder}
+            className="codeuiandy-image-border"
+          ></div>
+        </div>
       </div>
     </div>
   );
