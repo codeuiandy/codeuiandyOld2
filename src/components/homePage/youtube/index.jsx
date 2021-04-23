1d43df172af97d788564970be1f9713997c31382
Abydin <Abideenayinde7@gmail.com>
2021-04-23 16:20:20 +0100

    added youtube image section

--- src/components/homePage/youtube/index.jsx
+++ src/components/homePage/youtube/index.jsx
@@ -1,6 +1,11 @@
 import React from "react";
+var youtubeThumbnail = require("youtube-thumbnail");
 
 const Index = () => {
+  var thumbnail = youtubeThumbnail(
+    "https://www.youtube.com/watch?v=H7ghOLrb3LI"
+  );
+
   return (
     <div className="codeuiandy-youtube-wrapper">
       <div className="codeuiandy-youtube-header-wrapper">
@@ -8,6 +13,54 @@ const Index = () => {
         <h4>
           <span>Subscribe To Channel</span>
         </h4>
+      </div>
+      <div className="codeuiandy-youtube-body">
+        <div className="codeuiandy-youtube-thumbnails-wrapper">
+          <div className="codeuiandy-youtube-thumbnail">
+            <img
+              src={thumbnail.default.url}
+              alt=""
+              className="codeuiandy-youtube-yhumbnail"
+            />
+          </div>
+          <div className="codeuiandy-youtube-thumbnail">
+            <img
+              src={thumbnail.default.url}
+              alt=""
+              className="codeuiandy-youtube-yhumbnail"
+            />
+          </div>
+          <div className="codeuiandy-youtube-thumbnail">
+            <img
+              src={thumbnail.default.url}
+              alt=""
+              className="codeuiandy-youtube-yhumbnail"
+            />
+          </div>
+          <div className="codeuiandy-youtube-thumbnail">
+            <img
+              src={thumbnail.default.url}
+              alt=""
+              className="codeuiandy-youtube-yhumbnail"
+            />
+          </div>
+          <div className="codeuiandy-youtube-thumbnail">
+            {" "}
+            <img
+              src={thumbnail.default.url}
+              alt=""
+              className="codeuiandy-youtube-yhumbnail"
+            />
+          </div>
+          <div className="codeuiandy-youtube-thumbnail">
+            {" "}
+            <img
+              src={thumbnail.default.url}
+              alt=""
+              className="codeuiandy-youtube-yhumbnail"
+            />
+          </div>
+        </div>
       </div>
     </div>
   );
