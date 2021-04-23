2f5396fe996b5d393cfe86181f98eed9b141da1b
Abydin <Abideenayinde7@gmail.com>
2021-04-23 12:56:57 +0100

    remove the slash on the youtube channel page

--- src/components/homePage/youtube/index.jsx
+++ src/components/homePage/youtube/index.jsx
@@ -4,7 +4,7 @@ const Index = () => {
   return (
     <div className="codeuiandy-youtube-wrapper">
       <div className="codeuiandy-youtube-header-wrapper">
-        <h2 className="codeuiandy-youtube-header">Top Youtube Videos </h2>\
+        <h2 className="codeuiandy-youtube-header">Top Youtube Videos </h2>
         <h4>
           <span>Subscribe To Channel</span>
         </h4>
